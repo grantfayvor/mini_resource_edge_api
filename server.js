09f6674b149e5336eeefd7869edb4f1595f1dad6
Harrison Favour <favour.harrison@tenece.com>
2019-08-21 14:25:20 +0100

    added mongoose connection to server

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 var express = require("express"),
   app = express(),
-  port = parseInt(process.env.PORT) || 9000,
+  mongoose = require("mongoose"),
+  config = require("./config"),
+  port = parseInt(process.env.PORT) || config.server.port || 9000,
   assetRoute = require("./src/routes/assetRoute");
-
 
 app.use('/api/asset', assetRoute);
 
 
 app.listen(port, "127.0.0.1", args => {
   console.log("started server on port - " + port);
+  mongoose.connect(`mongodb://${config.database.address + ':' + config.database.port}/${config.database.name}`, {
+    useNewUrlParser: true
+  }, error => {
+    if (error) return console.log(error);
+    console.log("mongodb connection was successful");
+  });
 });
 
