f1bf1ef679240120fddc453adebb41be504e07ea
Harrison Favour <favour.harrison@tenece.com>
2019-08-21 14:26:36 +0100

    abstract configuration file

---
+++ config.js
@@ -0,0 +1,16 @@
+const config = {
+  server: {
+    protocol: "http",
+    address: "127.0.0.1",
+    port: 9000
+  },
+  database: {
+    name: "resource_edge",
+    address: "127.0.0.1",
+    port: "27017",
+    username: "",
+    password: ""
+  }
+};
+
+module.exports = config;
