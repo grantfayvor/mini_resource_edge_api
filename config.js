2b8c99ea418fe763f62b13a2cf88ca2b65013a15
Harrison Favour <favour.harrison@tenece.com>
2019-08-22 09:10:22 +0100

    switched to modes for configuration file

--- config.js
+++ config.js
@@ -1,15 +1,33 @@
 const config = {
-  server: {
-    protocol: "http",
-    address: "127.0.0.1",
-    port: 9000
+  dev: {
+    server: {
+      protocol: "http",
+      address: "127.0.0.1",
+      port: 9000
+    },
+    database: {
+      protocol: "mongodb",
+      name: "resource_edge",
+      host: "127.0.0.1",
+      port: "27017",
+      username: "",
+      password: ""
+    }
   },
-  database: {
-    name: "resourceedge?retryWrites=true",
-    address: "emma",
-    port: "BZKjA95TDFjuW12L@lawyerpp-cluster-5faak.mongodb.net",
-    username: "",
-    password: ""
+  production: {
+    server: {
+      protocol: "http",
+      address: "127.0.0.1",
+      port: 9000
+    },
+    database: {
+      protocol: "mongodb+srv",
+      name: "resourceedge?retryWrites=true",
+      host: "lawyerpp-cluster-5faak.mongodb.net",
+      port: "",
+      username: "emma",
+      password: "BZKjA95TDFjuW12L"
+    }
   }
 };
 
